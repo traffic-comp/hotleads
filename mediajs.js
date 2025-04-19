f562ec327954bfc7d480c45a55ef6c9a94b00217
traffic-comp <workg6542@gmail.com>
2025-04-19 14:11:46 +0300

    Update mediajs.js

--- mediajs.js
+++ mediajs.js
@@ -37,13 +37,10 @@ const handleClick = async function (e) {
       openSkype('live:.cid.c9f4f23c9e68115f');
       break;
     case 'telegram':
-      const data = `${leadIp.ip}&${getUtmParams().ad}&${getUtmParams().pixel}&${
-        leadIp.country
-      }`;
-
+      const data = `${leadIp.ip}&${getUtmParams().ad}-${leadIp.country}`;
       const base = stringToBase64(data);
-      console.log(`tg://resolve?domain=hot_hot_leads_bot&start=${base}`);
-      window.location.href = `tg://resolve?domain=hot_hot_leads_bot&start=${base}`;
+      console.log(`tg://resolve?domain=hot_hot_leads_bot&start=${data}`);
+      window.location.href = `tg://resolve?domain=hot_hot_leads_bot&start=${data}`;
       break;
     case 'whatsapp':
       window.location.href = links[this.dataset.platform];
